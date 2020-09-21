6596148684439307ff571ef51c7f5eb6e869dc8e
Fabian Clasen <fabi.clasen@googlemail.com>
2020-09-21 21:26:15 +0200

    Initial Commit

---
+++ MONA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MONA
+//
+//  Created by Fabian Clasen on 21.09.20.
+//  Copyright © 2020 Fabian Clasen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
