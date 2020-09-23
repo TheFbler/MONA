4b154c37c493bb1f5e64d46765fc67e95c5951af
Fabian Clasen <fabi.clasen@googlemail.com>
2020-09-23 17:25:15 +0200

    Erste UI für das Slide In Menü und vorbereiten der ViewController

---
+++ MONA/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  MONA
+//
+//  Created by Fabian Clasen on 23.09.20.
+//  Copyright © 2020 Fabian Clasen. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
