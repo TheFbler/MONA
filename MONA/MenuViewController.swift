eb2c570fc697520e59a1b50346da9708641a3ba9
Fabian Clasen <fabi.clasen@googlemail.com>
2020-09-23 18:40:57 +0200

    Spezielle Transition für SideMenu

--- MONA/MenuViewController.swift
+++ MONA/MenuViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
+enum MenuType: Int {
+    case home
+    case camera
+    case profile
+}
+
 class MenuViewController: UITableViewController {
+
+    var didTapMenuType: ((MenuType) -> Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let menuType = MenuType(rawValue: indexPath.row) else { return }
+        dismiss(animated: true) { [weak self] in
+            print("Dismissing: \(menuType)")
+            self?.didTapMenuType?(menuType)
+        }
+    }
 }
--- MONA/OverviewViewController.swift
+++ MONA/OverviewViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class OverviewViewController: UIViewController {
 
+    let transiton = SlideInTransition()
+    var topView: UIView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func didTapMenu(_ sender: UIBarButtonItem) {
+        guard let overviewViewController = storyboard?.instantiateViewController(withIdentifier: "OverviewView") as? MenuViewController else { return }
+        overviewViewController.didTapMenuType = { menuType in
+            self.transitionToNew(menuType)
+        }
+        overviewViewController.modalPresentationStyle = .overCurrentContext
+        overviewViewController.transitioningDelegate = self
+        present(overviewViewController, animated: true)
+    }
 
+    func transitionToNew(_ menuType: MenuType) {
+        let title = String(describing: menuType).capitalized
+        self.title = title
+
+        topView?.removeFromSuperview()
+        switch menuType {
+        case .profile:
+            let view = UIView()
+            view.backgroundColor = .yellow
+            view.frame = self.view.bounds
+            self.view.addSubview(view)
+            self.topView = view
+        case .camera:
+            let view = UIView()
+            view.backgroundColor = .blue
+            view.frame = self.view.bounds
+            self.view.addSubview(view)
+            self.topView = view
+        default:
+            break
+        }
+    }
+
+}
+
+extension OverviewViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = true
+        return transiton
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = false
+        return transiton
+    }
 }
 
