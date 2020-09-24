fd657ea8b0fb1a8cf19ea1417ec7323466a38fbb
Fabian Clasen <fabi.clasen@googlemail.com>
2020-09-24 17:17:27 +0200

    Container verwenden um die anderen Views anzuzeigen

---
+++ MONA/ContainerViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ContainerViewController.swift
+//  MONA
+//
+//  Created by Fabian Clasen on 21.09.20.
+//  Copyright © 2020 Fabian Clasen. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    let transiton = SlideInTransition()
+    var topView: UIView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Beim Start die Übersicht anzeigen
+        let vc = self.storyboard!.instantiateViewController(withIdentifier: "OverviewViewController") as! OverviewViewController
+        self.view.addSubview(vc.view)
+        self.title = "Übersicht"
+    }
+
+    @IBAction func didSwipeRight(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            openMenu()
+        }
+    }
+    
+    @IBAction func didTapMenu(_ sender: UIBarButtonItem) {
+        openMenu()
+    }
+    
+    func openMenu() {
+        guard let menuViewController = storyboard?.instantiateViewController(withIdentifier: "MenuViewController") as? MenuViewController else { return }
+        menuViewController.didTapMenuType = { menuType in
+            self.transitionToNew(menuType)
+        }
+        menuViewController.modalPresentationStyle = .overCurrentContext
+        menuViewController.transitioningDelegate = self
+        present(menuViewController, animated: true)
+    }
+
+    func transitionToNew(_ menuType: MenuType) {
+        topView?.removeFromSuperview()
+        switch menuType {
+        case .overview:
+            let vc = self.storyboard!.instantiateViewController(withIdentifier: "OverviewViewController") as! OverviewViewController
+            self.view.addSubview(vc.view)
+            self.title = "Übersicht"
+        case .expenses:
+            let vc = self.storyboard!.instantiateViewController(withIdentifier: "ExpensesViewController") as! ExpensesViewController
+            self.view.addSubview(vc.view)
+            self.title = "Ausgaben"
+        case .category:
+            let vc = self.storyboard!.instantiateViewController(withIdentifier: "CategoryViewController") as! CategoryViewController
+            self.view.addSubview(vc.view)
+            self.title = "Kategorien"
+        case .earnings:
+            let vc = self.storyboard!.instantiateViewController(withIdentifier: "EarningsViewController") as! EarningsViewController
+            self.view.addSubview(vc.view)
+            self.title = "Einnahmen"
+        case .about:
+            let vc = self.storyboard!.instantiateViewController(withIdentifier: "AboutViewController") as! AboutViewController
+            self.view.addSubview(vc.view)
+            self.title = "Über"
+        default:
+            break
+        }
+    }
+
+}
+
+extension ContainerViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = true
+        return transiton
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = false
+        return transiton
+    }
+}
+
--- MONA/OverviewViewController.swift
+++ MONA/OverviewViewController.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  OverviewViewController.swift
 //  MONA
 //
-//  Created by Fabian Clasen on 21.09.20.
+//  Created by Fabian Clasen on 24.09.20.
 //  Copyright © 2020 Fabian Clasen. All rights reserved.
 //
 
@@ -10,74 +10,21 @@ import UIKit
 
 class OverviewViewController: UIViewController {
 
-    let transiton = SlideInTransition()
-    var topView: UIView?
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-    }
 
-    @IBAction func didSwipeRight(_ sender: UISwipeGestureRecognizer) {
-        if sender.state == .ended {
-            openMenu()
-        }
+        // Do any additional setup after loading the view.
     }
     
-    @IBAction func didTapMenu(_ sender: UIBarButtonItem) {
-        openMenu()
-    }
-    
-    func openMenu() {
-        guard let menuViewController = storyboard?.instantiateViewController(withIdentifier: "MenuViewController") as? MenuViewController else { return }
-        menuViewController.didTapMenuType = { menuType in
-            self.transitionToNew(menuType)
-        }
-        menuViewController.modalPresentationStyle = .overCurrentContext
-        menuViewController.transitioningDelegate = self
-        present(menuViewController, animated: true)
-    }
 
-    func transitionToNew(_ menuType: MenuType) {
-        topView?.removeFromSuperview()
-        switch menuType {
-        case .overview:
-            let vc = self.storyboard!.instantiateViewController(withIdentifier: "OverviewViewController") as! OverviewViewController
-            self.view.addSubview(vc.view)
-            self.title = "Übersicht"
-        case .expenses:
-            let vc = self.storyboard!.instantiateViewController(withIdentifier: "ExpensesViewController") as! ExpensesViewController
-            self.view.addSubview(vc.view)
-            self.title = "Ausgaben"
-        case .category:
-            let vc = self.storyboard!.instantiateViewController(withIdentifier: "CategoryViewController") as! CategoryViewController
-            self.view.addSubview(vc.view)
-            self.title = "Kategorien"
-        case .earnings:
-            let vc = self.storyboard!.instantiateViewController(withIdentifier: "EarningsViewController") as! EarningsViewController
-            self.view.addSubview(vc.view)
-            self.title = "Einnahmen"
-        case .about:
-            let vc = self.storyboard!.instantiateViewController(withIdentifier: "AboutViewController") as! AboutViewController
-            self.view.addSubview(vc.view)
-            self.title = "Über"
-        default:
-            break
-        }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
 
 }
-
-extension OverviewViewController: UIViewControllerTransitioningDelegate {
-    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transiton.isPresenting = true
-        return transiton
-    }
-
-    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        transiton.isPresenting = false
-        return transiton
-    }
-}
-
