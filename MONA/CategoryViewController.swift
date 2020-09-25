09142d7c1664ea1dbcc2334cf31441c3e3db2d58
Fabian Clasen <fabi.clasen@googlemail.com>
2020-09-25 18:33:13 +0200

    Erste Vorbereitungen TableView Kategorien

--- MONA/CategoryViewController.swift
+++ MONA/CategoryViewController.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var categorys = [Category]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let obj = Category()
+        //obj.name = "Test"
+        categorys.append(obj)
     }
 
+}
+
+extension CategoryViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categorys.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath);
+        cell.textLabel?.text = categorys[indexPath.row].name
+        return cell;
+    }
 }
